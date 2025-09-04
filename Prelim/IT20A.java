a08f6efa1c63b679bef0a36a23fb3ad4083a35f8
KhyleAhronn77 <kindomkhyleahron@gmail.com>
2025-09-04 15:39:55 +0800

    Initialize folder as git repository

---
+++ Prelim/IT20A.java
@@ -0,0 +1,10 @@
+
+package Prelim;
+
+
+public class IT20A {
+    public static void main(String[] arg) {
+        System.out.println("Hello World");
+    }
+    
+}
---
+++ it20_kindom/IT20_KINDOM.java
@@ -0,0 +1,13 @@
+
+package it20_kindom;
+
+
+public class IT20_KINDOM {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        
+    }
+    
+}
